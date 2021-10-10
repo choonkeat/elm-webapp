3ac25c444bd5d232b7f1282c10cb7a9ef4302c86
choonkeat <choonkeat@hobnob.ai>
2021-10-10 19:18:09 +0800

    remove obsolete old files

--- templates/wait-for-changes.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const fs = require('fs')
-const target = process.argv.reverse()[0]
-fs.watch(target, { recursive: true }, function (eventType, filename) {
-  console.log('[wait]', eventType, filename)
-  this.close()
-})
-console.log('[wait] until file changes in', target)
