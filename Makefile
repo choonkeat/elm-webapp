cfa7ceadebb861d78e50045991cdb54762b7f7fb
choonkeat <choonkeat@hobnob.ai>
2021-10-10 13:20:46 +0800

    refactor bin/elm-webapp
    just cp -r from a template src directory

--- Makefile
+++ Makefile
@@ -1,26 +1,2 @@
 docs:
 	elm make --docs=docs.json
-
-build:
-	WATCHING=false elm-auto-encoder-decoder src/Protocol.elm
-	egrep -v '^function clientContent|return dict\[key\]' bin/elm-webapp | pbcopy
-	(pbpaste; \
-	 printf "function clientContent(key) { const dict = "; \
-	 ./bin/generate-client-content; \
-	 printf "; return dict[key] }\n") > bin/elm-webapp
-
-template-generate-all:
-	make template-clean template-generate GENTYPE=application
-	make template-clean template-generate GENTYPE=document
-	make template-clean template-generate GENTYPE=element
-
-template-clean:
-	rm -rf templates/$(GENTYPE)
-
-template-generate: build
-	./bin/elm-webapp $(GENTYPE) templates/$(GENTYPE)
-	make -C templates/$(GENTYPE) install
-	ln -s ../../../src/Webapp templates/$(GENTYPE)/src/Webapp
-	grep -v choonkeat/elm-webapp templates/$(GENTYPE)/elm.json > templates/$(GENTYPE)/elm.json2
-	mv templates/$(GENTYPE)/elm.json2 templates/$(GENTYPE)/elm.json
-	make -C templates/$(GENTYPE) compile
---
+++ bin/elm-webapp.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+
+const dirName = path.dirname(__dirname)
+const binName = path.basename(process.argv[1])
+const clientType = process.argv[2]
+const dstName = process.argv[3]
+
+function logErrorIfAny(err) {
+    if (!err) return;
+    console.error(err)
+}
+
+function copy_file(srcFile, dstFile) {
+    console.log("copy", srcFile, dstFile, "...")
+    var rd = fs.createReadStream(srcFile);
+    rd.on("error", logErrorIfAny);
+    var wr = fs.createWriteStream(dstFile);
+    wr.on("error", logErrorIfAny);
+    wr.on("close", logErrorIfAny);
+    rd.pipe(wr);
+};
+
+function copy_recursively (src, dst, callback) {
+    fs.readdir(src, function (err, dirnames) {
+        if (err) return callback(err)
+        fs.mkdir(dst, function() { // ignore mkdir error; pass to copy_file report
+            dirnames.forEach(function(name) {
+                if (name === "package.json") return; // "certain files are always included, regardless of settings" 🤦
+                const srcPath = path.join(src, name)
+                const dstPath = path.join(dst, name)
+                fs.stat(srcPath, function(err, dirent) {
+                    if (dirent.isDirectory()) {
+                        copy_recursively(srcPath, dstPath, logErrorIfAny);
+                    } else {
+                        copy_file(srcPath, dstPath)
+                    }
+                })
+            })
+        })
+    })
+};
+
+function showUsage() {
+    console.error(`
+
+USAGE:
+
+    ${binName} <type> <target_directory>
+
+TYPE:
+
+    While the generated "src/Server.elm" is the same, you can choose
+    what kind of "src/Client.elm" to generate:
+
+        application             generates a standard "Browser.application"
+
+        document                generates a standard "Browser.document"
+
+        element                 generates a standard "Browser.element"
+
+        application-element     generates a standard "Browser.element" with
+                                routing capabilities like "Browser.application"
+                                but more compatible with browser extensions
+
+EXAMPLE:
+
+    ${binName} application helloworld
+
+    `);
+    process.exit(1)
+}
+
+if (typeof dirName === 'undefined' || typeof dstName === 'undefined') {
+    showUsage()
+} else {
+    copy_recursively(path.join(dirName, 'templates', clientType), dstName, showUsage)
+}
