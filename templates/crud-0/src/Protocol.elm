476a8047d8e43e39c5f71fa212266e237a1f5235
choonkeat <choonkeat@hobnob.ai>
2021-10-16 15:26:45 +0800

    copy of "crud" without foobar

---
+++ templates/crud-0/src/Protocol.elm
@@ -0,0 +1,73 @@
+module Protocol exposing (..)
+
+import Json.Encode
+import Url.Parser
+
+
+{-| Url path that Client sends MsgFromClient values to
+-}
+httpEndpoint : String
+httpEndpoint =
+    "/api/elm-webapp"
+
+
+{-| All messages that Client can send to Server
+-}
+type MsgFromClient
+    = ManyMsgFromClient (List MsgFromClient)
+    | SetGreeting String
+
+
+{-| All messages that Server can reply to Client
+-}
+type MsgFromServer
+    = ManyMsgFromServer (List MsgFromServer)
+    | ClientServerVersionMismatch Json.Encode.Value
+    | ShowAlert Alert
+    | RedirectTo Page
+    | CurrentGreeting String
+
+
+{-| Http headers will be parsed into a RequestContext
+Failure to parse means error; keep an always successful scenario, e.g. Anonymous
+-}
+type RequestContext
+    = Cookied String
+    | Anonymous
+
+
+
+--
+
+
+type alias Alert =
+    { title : String
+    , body : String
+    }
+
+
+clientServerMismatchAlert : Alert
+clientServerMismatchAlert =
+    Alert "Oops! Page has expired" "Please reload this page on your browser"
+
+
+type Page
+    = NotFoundPage
+    | HomePage
+
+
+pageRouter : Url.Parser.Parser (Page -> a) a
+pageRouter =
+    Url.Parser.oneOf
+        [ Url.Parser.map HomePage Url.Parser.top
+        ]
+
+
+pagePath : Page -> String
+pagePath page =
+    case page of
+        NotFoundPage ->
+            "/"
+
+        HomePage ->
+            "/"
